9e3a713719e222b2778e6777ecca90518f453fa4
jayanandshah4321 <jayanand95shah@gmail.com>
2024-04-10 09:55:36 +0530

    updated

--- index.js
+++ index.js
@@ -16,25 +16,30 @@ const db = mongoose.connection;
 db.on('error', () => console.log("Error in connecting to the Database"));
 db.once('open', () => console.log("Connected to Database"));
 
-app.post("/add", (req, res) => {
-    const category_select = req.body.category_select;
-    const amount_input = req.body.amount_input;
-    const info = req.body.info;
-    const date_input = req.body.date_input;
+app.post("/add", (req,res) =>{
+    var category_select = req.body.category_select
+    var amount_input= req.body.amount_input
+    var info = req.body.info
+    var date_input = req.body.date_input
 
-    const data = {
+    var data={
         "Category": category_select,
-        "Amount": amount_input,
+        "Amount" : amount_input,
         "Info": info,
         "Date": date_input
-    };
-    db.collection('users').insertOne(data, (err, collection) => {
-        if (err) {
+    }
+    db.collection('users').insertOne(data, (err,collection) => {
+        if(err){
+            console.log(err);
             throw err;
+            
         }
-        console.log("Record Inserted Successfully");
-    });
-});
+        console.log("Record Inserted Successfully from /add")
+
+    })
+})
+
+
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -65,7 +70,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
         // Insert data into MongoDB
         // Example:
-        // await db.collection('users').insertMany(data);
+        await db.collection('users').insertMany(data);
 
         res.status(200).json(data);
     } catch (error) {
